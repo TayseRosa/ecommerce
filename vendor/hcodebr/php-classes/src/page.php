7b32b185f8f30c7ff0bc68b5eff10d92d1192fdc
Tayse Rosa <tayse_rosa@yahoo.com.br>
2020-03-06 03:30:14 -0300

    Page Admin e TPL Admin

--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -10,13 +10,14 @@ class Page{
         "data"=>[]
     ];
 
-    public function __construct($opts = array()){
+    public function __construct($opts = array(), $tpl_dir = "/views/")
+    {
 
         $this->options = array_merge($this->defaults, $opts);
 
         // config
         $config = array(
-            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"] . "/views/",
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"] . $tpl_dir,
             "cache_dir"     => $_SERVER["DOCUMENT_ROOT"] . "/views-cache/",
             "debug"         => false // set to false to improve the speed
         );
@@ -43,7 +44,8 @@ class Page{
         return $this->tpl->draw($name, $returnHTML);
     }
 
-    public function __destruct(){
+    public function __destruct()
+    {
         $this->tpl->draw("footer");
     }
 }
